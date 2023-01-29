404affe48d462bd9aa5ef63a6d2d13c0aa300687
Abed Nahouli <abednahouli@gmail.com>
2023-01-29 17:54:08 +0200

    added exclamation marks

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Herstopia</p>
+        <p>Welcome to Herstopia!!</p>
         <p> user ID: {x}</p>
       </header>
     </div>
