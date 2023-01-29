36d2a2d91742031da839dad9c19805442d3a872d
Abed Nahouli <abednahouli@gmail.com>
2023-01-29 17:07:04 +0200

    github actions build first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useMemo } from "react";
+
+function App() {
+  const x = useMemo(() => Math.floor(Math.random() * 100 + 1), []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to Herstopia</p>
+        <p> user ID: {x}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
